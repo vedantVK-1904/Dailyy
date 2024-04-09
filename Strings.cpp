d016bd869e3a3839a0c9eaf529036a0f43b11b26
Vedant Kalandre <124573857+vedantVK-1904@users.noreply.github.com>
2024-04-09 12:26:46 +0530

    Create Strings.cpp

---
+++ Strings.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n; long L; char a; float F; double D;
+    scanf("%d %ld %c %f %lf", &n, &L, &a, &F, &D);
+   printf("%d\n%ld\n%c\n%f\n%lf", n, L, a, F, D);
+
+    
+    return 0;
+}
+
+//Input 3 12345678912345 a 334.23 14049.30493
+//Output 3 
+//12345678912345
+ //a 
+//334.23 1
+//4049.30493

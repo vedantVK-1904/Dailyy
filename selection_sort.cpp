280d4342940c41c8b5393fdf6210f2e9fa55738a
Vedant Kalandre <vedant.kalandre0@gmail.com>
2024-10-02 14:21:37 +0530

    added <array> method

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include<array>
 int* selection_sort(int input_array[], int array_size) {
     for (int i = 0; i < array_size - 1; i++) {
         for (int j = i + 1; j < array_size; j++) {
@@ -13,6 +13,21 @@ int* selection_sort(int input_array[], int array_size) {
     return input_array;  // Return pointer to the sorted array
 }
 
+void selection_sort(std::array<int, 10>& Arr){
+
+    for (int i = 0; i < Arr.size(); i++){
+        for (int j = i + 1; j < Arr.size(); j++){
+            if (Arr[i] > Arr[j]){
+                int temp = Arr[i];
+                Arr[i] = Arr[j];
+                Arr[j] = temp;
+            }   
+        }   
+    }
+
+}
+
+
 int main() {
     int input_array[] = {64, 25, 12, 22, 11};
     int array_size = sizeof(input_array) / sizeof(input_array[0]);
@@ -25,6 +40,16 @@ int main() {
         std::cout << sorted_array[i] << " ";
     }
     std::cout << std::endl;
+
+
+std::array <int, 10> arr = {19,20,13,14,5,85,1,31,40,3};
+    selection_sort(arr);
+    for (int i = 0; i < 10; i++){
+
+        std::cout <<" Sorted Array " << arr[i] << "\n";
+
+    }
+
 
     return 0;
 }
